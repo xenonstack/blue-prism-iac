df4bea88488e72b0ac1b2e800982b678db661a3a
Gursimran Singh <gursimran@xenonstack.com>
2019-09-29 00:58:50 +0530

    Update push.ps1

--- push.ps1
+++ push.ps1
@@ -1,7 +1,8 @@
 param(
 	[parameter()]
 	[string[]]$releasename,
-	[string[]]$tag
+	[string[]]$tag,
+	[string[]]$git_branch
 )
 
 
@@ -10,5 +11,5 @@ $tagged_release = "${releasename}-${tag}.bprelease"
 
 
 git add $tagged_release
-git commit -m "Commit by Jenkins for Build"
-git push
+git commit -m "Commit by Jenkins for Build $tag"
+git push origin $git_branch
