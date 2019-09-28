1180945c5ea55e49c92474f1b82095f24f7a25f6
Gursimran Singh <gursimran@xenonstack.com>
2019-09-29 00:39:36 +0530

    Update tag.ps1

--- tag.ps1
+++ tag.ps1
@@ -6,15 +6,17 @@ param(
 Write-Host $releasename
 Write-Host $tag
 
+$tagged_release = "${releasename}-${tag}.bprelease"
+Write-Host $tagged_release
+
+$releasename = "${releasename}.bprelease"
+Write-Host $releasename
 
 $WantFile = ".\$releasename"
 Write-Host $WantFile
+
 $FileExists = Test-Path $WantFile
 Write-Host $FileExists
-$tagged_release = "${releasename}-${tag}.bprelease"
-Write-Host $tagged_release
-$releasename = "${releasename}.bprelease"
-Write-Host $releasename
 
 
 If ($FileExists -eq $True) {
