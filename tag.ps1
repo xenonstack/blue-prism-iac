750727330a237a826b6a97521d8db00fa0b0a9a3
Gursimran Singh <gursimran@xenonstack.com>
2019-09-29 00:41:25 +0530

    Update tag.ps1

--- tag.ps1
+++ tag.ps1
@@ -3,26 +3,18 @@ param(
 	[string[]]$releasename,
 	[string[]]$tag
 )
-Write-Host $releasename
-Write-Host $tag
+
+
 
 $tagged_release = "${releasename}-${tag}.bprelease"
-Write-Host $tagged_release
-
 $releasename = "${releasename}.bprelease"
-Write-Host $releasename
-
 $WantFile = ".\$releasename"
-Write-Host $WantFile
-
 $FileExists = Test-Path $WantFile
-Write-Host $FileExists
 
 
 If ($FileExists -eq $True) {
-& "cp $releasename $tagged_release"
+& "cp .\$releasename .\$tagged_release"
 }
-
 Else {
 Write-Host "Could Not Tag Release Artifact."
 }
